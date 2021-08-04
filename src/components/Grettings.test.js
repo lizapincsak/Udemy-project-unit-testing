bacab0d6f2c1e830ebf52c947856ca1032319292
lizapincsak <liza.pincsak@gmail.com>
2021-08-03 21:07:13 -0500

    first test

---
+++ src/components/Grettings.test.js
@@ -0,0 +1,16 @@
+
+import { render, screen } from '@testing-library/react';
+import Greetings from './Greetings';
+
+test('renders Hello World as a text', () => {
+    //Arrange
+    render(<Greetings/>);
+
+    //Act
+    //...nothing
+
+    //Assert
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+
+});
